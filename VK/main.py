f9e9a63dcfe33e2947c93abe0f430e6b8ceac706
ivan-nev <i_nevakshenov@mail.ru>
2022-10-01 23:56:59 +0500

    add keyboard

--- VK/main.py
+++ VK/main.py
@@ -1,48 +1,54 @@
-import json
 from VK.class_VKapp import VKapp
-from vk_api.longpoll import VkEventType
 import config
+import json
+from vk_api.longpoll import VkEventType
 from pprint import pprint
-# from keyboard import sender
+from VK.keyboard import send_kb, send_kb_in_message
+
+
+vkapp = VKapp(token_user=config.vk_token_prog, tokenVK_Group=config.vk_token_my)
+
+
 
 
 
 if __name__ == '__main__':
-    bot = VKapp(token_user=config.vk_token_prog, tokenVK_Group=config.vk_token_my)
-    # a = bot.foto_dict(1)
-    # print(a)
+    # bot = VKapp(token_user=config.vk_token_prog, tokenVK_Group=config.vk_token_my)
+    a = vkapp.foto_dict(1)
+    print(a)
     # print(list(bot.total_dict(54934926))[0])
     def create_file(total_dict):
         with open(f"total.json", "w",encoding='UTF-8') as write_file:
             json.dump(total_dict, write_file)
         print(f'Создан файл: total.json')
-    for event in bot.longpoll.listen():
+    for event in vkapp.longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
                 msg = event.text.lower()
                 user_id = event.user_id
-                # sender(user_id, msg.lower())
+                send_kb(user_id, msg.lower())
                 if msg == 'hi':
-                    bot.send_msg(user_id, 'hellow froend')
-                    bot.send_msg(user_id, 'how a u?')
+                    vkapp.send_msg(user_id, 'hellow froend')
+                    vkapp.send_msg(user_id, 'how a u?')
                 if msg == 'ok':
-                    bot.send_msg(user_id, 'tell the amoun of 5 and 5)')
-                    bot.send_msg(user_id, user_id)
+                    vkapp.send_msg(user_id, 'tell the amoun of 5 and 5)')
+                    vkapp.send_msg(user_id, user_id)
                 if msg == 'name':
-                    bot.send_msg(user_id, bot.get_name(user_id))
+                    vkapp.send_msg(user_id, vkapp.get_name(user_id))
                 if msg == 'adge':
-                    bot.send_msg(user_id, bot.get_age(user_id))
+                    vkapp.send_msg(user_id, vkapp.get_age(user_id))
                 if msg == 'city':
-                    bot.send_msg(user_id, bot.get_id_city(user_id))
+                    vkapp.send_msg(user_id, vkapp.get_id_city(user_id))
                 if msg == 'sex':
-                    bot.send_msg(user_id, bot.get_reverse_sex(user_id))
+                    vkapp.send_msg(user_id, vkapp.get_reverse_sex(user_id))
                 if msg == 'search':
-                    total_dict = bot.total_dict(user_id)
-                    bot.send_msg(user_id, f'https://vk.com/id{(list(total_dict)[0])}')
-                    bot.send_foto(user_id=user_id, user_id_foto=(list(total_dict)[0]))
-                    bot.send_msg(user_id, f'https://vk.com/id{(list(total_dict)[1])}')
-                    bot.send_foto(user_id=user_id, user_id_foto=(list(total_dict)[1]))
-                    bot.send_msg(user_id, f'https://vk.com/id{(list(total_dict)[2])}')
-                    bot.send_foto(user_id=user_id, user_id_foto=(list(total_dict)[2]))
+                    total_dict = vkapp.total_dict(user_id)
+                    vkapp.send_msg(user_id, f'https://vk.com/id{(list(total_dict)[0])}')
+                    send_kb_in_message(user_id, msg.lower(),(list(total_dict)[0]))
+                    vkapp.send_foto(user_id=user_id, user_id_foto=(list(total_dict)[0]))
+                    vkapp.send_msg(user_id, f'https://vk.com/id{(list(total_dict)[1])}')
+                    vkapp.send_foto(user_id=user_id, user_id_foto=(list(total_dict)[1]))
+                    vkapp.send_msg(user_id, f'https://vk.com/id{(list(total_dict)[2])}')
+                    vkapp.send_foto(user_id=user_id, user_id_foto=(list(total_dict)[2]))
                     print(total_dict)
                     create_file(total_dict)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from vk_api.longpoll import VkEventType
 from DB.class_DBapp import DBapp
 from DB.db_models import create_tables, Match, User, FavoriteList, UnFavoriteList, SearchingList
 from VK.class_VKapp import VKapp
+from VK.keyboard import send_kb, send_kb_in_message
 
 conn = psycopg2.connect(database=config.db_name, user=config.db_login, password=config.db_password)
 DSN = f'postgresql://{config.db_login}:{config.db_password}@localhost:5432/{config.db_name}'
@@ -25,7 +26,7 @@ if __name__ == '__main__':
             if event.to_me:
                 msg = event.text.lower()
                 user_id = event.user_id
-                # sended_keybord(user_id, msg.lower())
+                send_kb(user_id, msg.lower())
                 if msg == 'hi':
                     vkapp.send_msg(user_id, 'hello friend')
                     vkapp.send_msg(user_id, 'how a u?')
@@ -48,16 +49,34 @@ if __name__ == '__main__':
                                    f'{result[1]} {result[2]}\n'
                                    f'https://vk.com/id{result[0]}\n'
                                    )
+                    send_kb_in_message(user_id, msg.lower(), result[0])
                     vkapp.send_foto(user_id=user_id, user_id_foto=result[0])
                     result = dbapp.get_next_search()
                     vkapp.send_msg(user_id,
                                    f'{result[1]} {result[2]}\n'
                                    f'https://vk.com/id{result[0]}\n'
                                    )
+                    send_kb_in_message(user_id, msg.lower(), result[0])
                     vkapp.send_foto(user_id=user_id, user_id_foto=result[0])
                     result = dbapp.get_next_search()
                     vkapp.send_msg(user_id,
                                    f'{result[1]} {result[2]}\n'
                                    f'https://vk.com/id{result[0]}\n'
                                    )
+                    send_kb_in_message(user_id, msg.lower(), result[0])
                     vkapp.send_foto(user_id=user_id, user_id_foto=result[0])
+                elif msg.split('_')[0] == 'black':
+                    # код для вставки анкеты с ID {msg.split('_')[1]} в чёрный список
+                    print(f"BLACK ID {msg.split('_')[1]}")
+                elif msg.split('_')[0] == 'favorite':
+                    # код для вставки анкеты с ID {msg.split('_')[1]} в избранное
+                    print(f"Favorite ID {msg.split('_')[1]}")
+                elif msg == "<back":
+                    pass #переход к предыдущим анкетам
+                elif msg == 'next>':
+                    pass # сл анкеты
+                elif msg == 'view_favorite':
+                    pass # показать избранное
+                elif msg == 'view_black':
+                    pass # Показать черный список
+
